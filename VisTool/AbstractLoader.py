9f40f7fa1c253814481de213295ff1c944ed2d7b
Johannes Waschke <waschke@informatik.uni-leipzig.de>
2020-01-13 16:58:38 +0100

    Refactored tracklist-to-matrix-converter

--- VisTool/AbstractLoader.py
+++ VisTool/AbstractLoader.py
@@ -5,15 +5,16 @@ from scipy.ndimage import zoom
 class AbstractLoader:
     def __init__(self, resampleTo, minTrackLength):
         """ Initialization with basic members.
+            Child classes must fill self.trackList, self.dim and 
+            self.attributeNames!
         """
         self.trackList = {}
-        self.tracksNp = np.empty((0))
-        self.attributesNp = np.empty((0))
+        self.tracksMatrix = np.empty((0))
+        self.attributesMatrix = np.empty((0))
         self.attributeNames = []
         self.resampleTo = resampleTo
         self.dim = 3
         self.minTrackLength = minTrackLength
-        # Child class must fill self.trackList, self.dim and self.attributeNames!
 
     def trackListToNumpy(self):
         """ Converts the internal list (dictionary trackId -> track, where
@@ -21,48 +22,63 @@ class AbstractLoader:
             a numpy array and resizes them to self.resampleTo.
             Requries self.dim and self.attributeNames to be properly filled.
         """
-        self.tracksNp = np.zeros(
-            (len(self.trackList.keys()), self.resampleTo, self.dim))
-        self.attributesNp = np.zeros(
-            (len(self.trackList.keys()), self.resampleTo, len(self.attributeNames)))
-        counter = 0
+        self.initEmptyTrackMatrix()
+        counterValidTracks = 0
         print("Convert tracks to numpy array")
-        for i in self.trackList.keys():
-            trackNp = np.zeros((len(self.trackList[i]), 3))
-            # TODO: handle attributes?
-            attributesNp = np.zeros(
-                (len(self.trackList[i]), len(self.attributeNames)))
-            for j in range(len(self.trackList[i])):
-                trackNp[j, 0] = self.trackList[i][j][0]
-                trackNp[j, 1] = self.trackList[i][j][1]
-                trackNp[j, 2] = self.trackList[i][j][2]
-                for a in range(len(self.attributeNames)):
-                    attributesNp[j, a] = self.trackList[i][j][self.dim + a]
-            scale = float(self.resampleTo) / len(trackNp)
-
-            # Resize the track, if valid size
-            if len(trackNp) > self.minTrackLength:
-                trackNpZoomed = zoom(trackNp, (scale, 1))
-                if len(trackNpZoomed) != self.resampleTo:
-                    print("Unexpected track size after resampling:",
-                          len(trackNpZoomed))
-                attributesNpZoomed = zoom(attributesNp, (scale, 1))
-
-                # TODO: a bug - sometimes zoom produces zero values in the last row.
-                # Copy first and last to always keep original start and end points
-                trackNpZoomed[0] = trackNp[0]
-                attributesNpZoomed[0] = attributesNp[0]
-                trackNpZoomed[-1] = trackNp[-1]
-                attributesNpZoomed[-1] = attributesNp[-1]
-                self.tracksNp[counter] = trackNpZoomed
-                self.attributesNp[counter] = attributesNpZoomed
-                self.simpleStatusPrint(counter, 50)
-                counter += 1
+        for trackId in self.trackList.keys():
+            track, attributes = self.trackFromListToMatrix(trackId)
+            if len(track) > self.minTrackLength:
+                track, attributes = self.resizeTrack(track, attributes)
+                self.addTrackToMatrix(counterValidTracks,
+                                      track, attributes)
+                self.simpleStatusPrint(counterValidTracks, 50)
+                counterValidTracks += 1
             else:
-                self.tracksNp = self.tracksNp[0:(len(self.tracksNp)-1)]
-                self.attributesNp = self.attributesNp[0:(
-                    len(self.attributesNp)-1)]
+                self.shrinkTrackMatrix()
         print()
+
+    def trackFromListToMatrix(self, index):
+        track = np.zeros((len(self.trackList[index]), 3))
+        attributes = np.zeros(
+            (len(self.trackList[index]), len(self.attributeNames)))
+        for j in range(len(self.trackList[index])):
+            track[j, 0] = self.trackList[index][j][0]
+            track[j, 1] = self.trackList[index][j][1]
+            track[j, 2] = self.trackList[index][j][2]
+            for a in range(len(self.attributeNames)):
+                attributes[j, a] = self.trackList[index][j][self.dim + a]
+        return track, attributes
+
+    def initEmptyTrackMatrix(self):
+        self.tracksMatrix = np.zeros(
+            (len(self.trackList.keys()), self.resampleTo, self.dim))
+        self.attributesMatrix = np.zeros(
+            (len(self.trackList.keys()), self.resampleTo, len(self.attributeNames)))
+
+    def addTrackToMatrix(self, position, track, attributes):
+        self.tracksMatrix[position] = track
+        self.attributesMatrix[position] = attributes
+
+    def shrinkTrackMatrix(self):
+        """ Removes the last column of the track/attribute matrices,
+            e.g. after a track was deleted """
+        self.tracksMatrix = self.tracksMatrix[0:(len(self.tracksMatrix)-1)]
+        self.attributesMatrix = self.attributesMatrix[0:(len(self.attributesMatrix)-1)]
+
+    def resizeTrack(self, track, attributes):
+        scale = float(self.resampleTo) / len(track)
+        trackZoomed = zoom(track, (scale, 1))
+        attributesZoomed = zoom(attributes, (scale, 1))
+        if len(trackZoomed) != self.resampleTo:
+            print("Warning: Unexpected length after resampling:", len(trackZoomed))
+
+        # Copy first and last to always keep original start and end points,
+        # avoiding interpolation artifacts.
+        trackZoomed[0] = track[0]
+        attributesZoomed[0] = attributes[0]
+        trackZoomed[-1] = track[-1]
+        attributesZoomed[-1] = attributes[-1]
+        return trackZoomed, attributesZoomed
 
     def simpleStatusPrint(self, i=1, sparse=1):
         """ Prints a star (*) if for each call (or only if i % sparse == 0, to only print 
@@ -76,4 +92,4 @@ class AbstractLoader:
     def get(self):
         """ get the three lists: tracks, attribute (values), attribute names 
         """
-        return self.tracksNp, self.attributesNp, self.attributeNames
+        return self.tracksMatrix, self.attributesMatrix, self.attributeNames
